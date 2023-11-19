deeccb56018fad397685c99d20f1c46d956bd2c7
Vlad <v.katk0ov@gmail.com>
2023-11-19 12:21:22 +0200

    script

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ let result = document.getElementById('result');
 let day = new Date().getDate();
 
 function calc() {
-    let priceDay = num.value / 30
-    result.innerHTML = `-${Math.round(priceDay * (30 - day + 1))} грн.`;
+    if (num.value < 0) {
+        result.innerHTML = 'Невірний формат тарифу'
+    } else {
+        let priceDay = num.value / 30
+        if (day == 31) {
+            day -= 1;
+        };
+        result.innerHTML = `-${Math.round(priceDay * (30 - day + 1))} грн.`;
+    }
 }
 
 btn.addEventListener('click', calc)
